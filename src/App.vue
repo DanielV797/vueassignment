0c80834b9cd2a5c333a6c146784ec0989ae32a90
Daniël Verbaan <verbaan97@outlook.com>
2021-03-29 17:18:10 +0200

    Added McSmurry

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <McSmurry msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import McSmurry from './components/McSmurry.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    McSmurry
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/McSmurry.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="smurry">
+    <h1>Invoer</h1>
+    
+    <input v-model="code" minlength="6" maxlength="6"/>
+    <br>
+    <br>
+    <button v-on:click="sendCode">Smurrify</button>
+
+    <!-- <h1>Uitvoer</h1> https://youtu.be/fsBJXiP--Jc -->
+    <p> Jouw uitvoer is: {{ code }} </p>
+        <h5>IJs: {{ flavour }}</h5>
+        <h5>Choco: {{ choco }}</h5>
+        <h5>Snoep: {{ candy }}</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Smurry',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      code: '',
+      flavour: code.slice(0,2),
+      choco: code.slice(2,4),
+      candy: code.slice(4,6),
+      
+
+      ass: {
+        flavour: { 12: 'Vanille', 13: 'Chocolade', 21: 'Aardbei', 25: 'Banaan' },
+        choco: { 44: 'Mars', 13: 'Twix', 52: 'Snickers', 21: 'Bounty' },
+        candy: { 12: 'Kikkertjes', 13: 'Winegums', 23: 'Gummibeertjes', 99: 'Kauwgumballen' },
+      }
+    }
+  },
+  methods: {
+    sendCode() {
+      /* eslint-disable no-debugger */
+      //debugger;
+      /* eslint-enable no-debugger */
+      console.log(this.code);
+      console.log(this.code.slice(0,2));
+      console.log(this.code.slice(2,4));
+      console.log(this.code.slice(4,6));
+
+
+
+
+
+      
+    }
+  }
+
+}
+</script>
